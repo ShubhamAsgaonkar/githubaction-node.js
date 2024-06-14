6e491b95f3c29e53138daab7569cf2c8857eeb5d
Shubham_Asgaonkar <95036340+ShubhamAsgaonkar@users.noreply.github.com>
2024-06-14 19:51:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "5.53.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-south-1"
+  access_key = var.aws-access_key
+  secret_key = var.aws-secret_key
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0f58b397bc5c1f2e8"
+    instance_type = "t2.micro"
+    tags = "nodejs"
+  
+}
